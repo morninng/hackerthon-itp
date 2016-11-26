46d329d60737bfb0ed55d4373c128bcac53c64ce
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-11-26 20:36:50 +0900

    server info is shown on the web

--- src/app/lesson-layout/lesson-layout.component.ts
+++ src/app/lesson-layout/lesson-layout.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit,ChangeDetectorRef } from '@angular/core';
 import {SkywayService} from './../service/skyway.service'
 
 import {RecognitionService} from './../service/recognition.service'
+import { AngularFire } from 'angularfire2';
 
 
 @Component({
@@ -14,10 +15,12 @@ export class LessonLayoutComponent implements OnInit {
 
   room_users = [];
   stream_data={};
+  lesson_data={};
 
   constructor(private skyway: SkywayService,
               private change_ref: ChangeDetectorRef,
-              private recognition: RecognitionService) { }
+              private recognition: RecognitionService,
+              private af: AngularFire) { }
 
   ngOnInit() {
     this.skyway.room_data_subject.subscribe((room_data)=>{
@@ -27,6 +30,14 @@ export class LessonLayoutComponent implements OnInit {
       this.change_ref.markForCheck();
     })
     this.recognition.start();
+
+
+    const item = this.af.database.object('/hackerthon-ipt/', { preserveSnapshot: true });
+    item.subscribe((snapshot)=>{
+      this.lesson_data = snapshot.val() || {};
+      console.log(this.lesson_data);
+    })
+
   }
 
 }
